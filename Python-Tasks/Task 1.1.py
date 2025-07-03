9573ab80b340d00c2fd9554c5f91aa36aa0f7b15
TasneemAbdelrahman <168738464+TasneemAbdelrahman@users.noreply.github.com>
2025-07-03 10:26:02 +0300

    Add files via upload

---
+++ Python-Tasks/Task 1.1.py
@@ -0,0 +1,10 @@
+#This code contains my name,age and country. 
+name="Name:Tasneem"
+age="Age:19"
+country="Country:Deirmawas"
+print(name)
+print(age)
+print(country) 
+print(type(name)) 
+print(type(age))
+print(type(country))
