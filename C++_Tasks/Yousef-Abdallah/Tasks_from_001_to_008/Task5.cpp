d43d04f1746aab264512d9573079d136cdfe9759
Yousef-Abdallah1 <ya2505052@gmail.com>
2025-07-03 18:33:42 +0300

    Create Task5.cpp

---
+++ C++_Tasks/Yousef-Abdallah/Tasks_from_001_to_008/Task5.cpp
@@ -0,0 +1,13 @@
+// This Is My First File
+// In Learning Programming
+
+#include <iostream> /* Preprocessor Directive */
+
+int main()
+{
+	std::cout /* Character Output */ << "Line One\n";
+	std::cout << "Line Two\n"; // cout Function
+	std::cout << "Line Three\n";
+
+	return 0;
+} // End Main Function
