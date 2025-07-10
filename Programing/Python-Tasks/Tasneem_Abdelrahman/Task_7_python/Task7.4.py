79828485391ea24e56f8227015ab89ff0daf926b
TasneemAbdelrahman <168738464+TasneemAbdelrahman@users.noreply.github.com>
2025-07-10 23:31:33 +0300

    Add files via upload

---
+++ Programing/Python-Tasks/Tasneem_Abdelrahman/Task_7_python/Task7.4.py
@@ -0,0 +1,11 @@
+num_one = 10
+num_two = 20
+result=num_one+num_two
+print(result)
+result**=3
+print(result) 
+result%=26000
+print(result)
+result/=5
+print(float(result)) 
+print(type(str(result)))
